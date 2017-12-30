3eb3f638e9bfb1779772c0e98b5115b865ce3059
rexfordnana <rexfordnana@gmail.com>
2017-12-30 13:20:59 -0500

    Created post detail template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -8,5 +8,10 @@ def post_list(request):
     # posts = Post.objects.filter(published_date__gte=timezone.now()).order_by('published_date')
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
